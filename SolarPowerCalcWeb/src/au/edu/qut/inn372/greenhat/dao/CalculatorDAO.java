20ce1f83be86cf4730215929bbd4b453411da059
charlestontelles <charlestontelles@gmail.com>
2012-08-19 14:36:34 +1000

    added comments

--- SolarPowerCalcWeb/src/au/edu/qut/inn372/greenhat/dao/CalculatorDAO.java
+++ SolarPowerCalcWeb/src/au/edu/qut/inn372/greenhat/dao/CalculatorDAO.java
@@ -1,7 +1,12 @@
 package au.edu.qut.inn372.greenhat.dao;
 
 import au.edu.qut.inn372.greenhat.bean.Calculator;
-
+/**
+ * Data Access Object to provide operations related to the Calculator
+ * 
+ * @author Charleston
+ *
+ */
 public interface CalculatorDAO {
 	public void saveCalculation(Calculator calculator);
 	public void deleteCalculation (Calculator calculator);
